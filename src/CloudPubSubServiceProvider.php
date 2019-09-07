3493a19cf95ccdcd303dc2efeda380d3fc40f7ed
Amrani Houssain <amranidev@gmail.com>
2019-09-07 15:25:03 +0100

    Update CloudPubSubServiceProvider.php

--- src/CloudPubSubServiceProvider.php
+++ src/CloudPubSubServiceProvider.php
@@ -2,14 +2,9 @@
 
 namespace GDGTangier\PubSub;
 
-use Illuminate\Queue\QueueManager;
 use Illuminate\Support\ServiceProvider;
-use GDGTangier\PubSub\Subscriber\JobsMap;
-use GDGTangier\PubSub\Publisher\PublisherManager;
-use GDGTangier\PubSub\Subscriber\SubscriberConnector;
-use GDGTangier\PubSub\Publisher\Console\Commands\PublishMessage;
-use GDGTangier\PubSub\Subscriber\Console\Commands\SubscribeCommand;
-use GDGTangier\PubSub\Subscriber\Console\Commands\SubscriberMakeCommand;
+use GDGTangier\PubSub\Publisher\PublisherServiceProvider;
+use GDGTangier\PubSub\Subscriber\SubscriberServiceProvider;
 
 class CloudPubSubServiceProvider extends ServiceProvider
 {
@@ -20,11 +15,7 @@ class CloudPubSubServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->commands([
-            PublishMessage::class,
-            SubscribeCommand::class,
-            SubscriberMakeCommand::class,
-        ]);
+        //
     }
 
     /**
@@ -34,62 +25,7 @@ class CloudPubSubServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerPublisherManager();
-        $this->registerPublisherConnection();
-        $this->registerJobsMap();
-        $this->addSubscriberConnector();
-    }
-
-    /**
-     * Register the subscriber.
-     */
-    protected function addSubscriberConnector()
-    {
-        $this->app->afterResolving(QueueManager::class, function (QueueManager $manager) {
-            $manager->addConnector('pubsub', function () {
-                return new SubscriberConnector();
-            });
-        });
-    }
-
-    /*
-     * Register PublisherManager.
-     *
-     * return @void
-     */
-    protected function registerPublisherManager()
-    {
-        $this->app->singleton('gcloud.publisher', function ($app) {
-            return new PublisherManager($app);
-        });
-    }
-
-    /**
-     * Register publisher connection.
-     *
-     * @return void
-     */
-    public function registerPublisherConnection()
-    {
-        $this->app->singleton('gcloud.publisher.connection', function ($app) {
-            /** @var \GDGTangier\PubSub\Publisher\PublisherManager $manager */
-            $manager = $app['gcloud.publisher'];
-
-            return $manager->connection();
-        });
-    }
-
-    /**
-     * Register JobsMap.
-     *
-     * return @void
-     */
-    protected function registerJobsMap()
-    {
-        $this->app->singleton('gcloud.subscriber.map', function () {
-            $jobs = config('pubsub.subscriptions');
-
-            return new JobsMap($jobs);
-        });
+        $this->app->register(PublisherServiceProvider::class);
+        $this->app->register(SubscriberServiceProvider::class);
     }
 }
