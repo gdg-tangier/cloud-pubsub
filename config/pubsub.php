0f9a7a3252b72d6755633344c6e4511ec2f3dc3f
Amrani Houssain <amranidev@gmail.com>
2019-09-14 01:01:07 +0100

    Update pubsub.php

--- config/pubsub.php
+++ config/pubsub.php
@@ -10,14 +10,20 @@ return [
     ],
 
     /*
-     * Here where you can define events name with a topic.
+     * Here where you map events name with Google Pub/Sub topics.
+     *
+     * Means, map each event name to specific Google Pub/Sub topic.
      */
     'events'      => [
         'event_name' => '__YOUR_TOPIC_NAME__',
     ],
 
     /*
-     * Here where you can tie the subscriptions classes (jobs) to topics.
+     * Here where you can tie the subscription classes (jobs) to topics.
+     *
+     * Means, map each subscription job to a specific Google pubsub topic.
+     * The subscription job is responsible for handling the incoming data
+     * from a Google Pub/Sub topic.
      */
     'subscriptions'        => [
         \App\PubSub\DummyJob::class => '__YOUR_TOPIC_NAME__',
