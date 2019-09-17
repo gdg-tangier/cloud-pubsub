7608eb478074a1512dfb4485f6d4bb65bc3435bf
Amrani Houssain <amranidev@gmail.com>
2019-09-17 23:30:37 +0100

    Update PublisherTest.php

--- tests/Publisher/PublisherTest.php
+++ tests/Publisher/PublisherTest.php
@@ -3,24 +3,17 @@
 namespace GDGTangier\PubSub\Publisher;
 
 use GDGTangier\PubSub\Publisher\Exceptions\TopicNotFound;
+use GDGTangier\PubSub\Publisher\Facades\PublisherFacade;
 
 class PublisherTest extends \GDGTangier\PubSub\Tests\TestCase
 {
-    /** @var \Google\Cloud\PubSub\PubSubClient */
-    public $client;
-
-    /** @var \GDGTangier\PubSub\Publisher\Publisher */
-    public $publisher;
-
     /**
      * Setup.
      */
     protected function setUp(): void
     {
         parent::setUp();
-        $this->publisher = app('gcloud.publisher.connection');
-        $this->client = $this->publisher->getClient();
-        $this->publisher->getClient()->createTopic(self::TOPIC_NAME);
+        $this->setUpPubSub();
     }
 
     /**
@@ -29,7 +22,7 @@ class PublisherTest extends \GDGTangier\PubSub\Tests\TestCase
     protected function tearDown(): void
     {
         parent::tearDown();
-        $this->client->topic(self::TOPIC_NAME)->delete();
+        $this->deletePubSub();
     }
 
     /**
@@ -40,6 +33,14 @@ class PublisherTest extends \GDGTangier\PubSub\Tests\TestCase
     public function can_publish_a_message()
     {
         $response = $this->publisher->publish('DummyMessage', self::TOPIC_NAME);
+
+        self::assertTrue(count($response['messageIds']) != 0);
+    }
+
+    /** @test */
+    public function can_publish_a_message_trough_facade()
+    {
+        $response = PublisherFacade::publish('DummyMessage', self::TOPIC_NAME);
 
         self::assertTrue(count($response['messageIds']) != 0);
     }
