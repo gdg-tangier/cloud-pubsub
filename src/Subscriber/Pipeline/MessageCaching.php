94a43cc1cd986310c99b79f44a03a4f26eb6be4c
Amrani Houssain <amranidev@gmail.com>
2019-10-10 21:26:17 +0100

    Formatting

--- src/Subscriber/Pipeline/MessageCaching.php
+++ src/Subscriber/Pipeline/MessageCaching.php
@@ -8,11 +8,12 @@ class MessageCaching
      * Handle the incoming message.
      *
      * @param \Google\Cloud\PubSub\Message $message
-     * @param \Closure                     $next
-     *
-     * @throws \Illuminate\Container\EntryNotFoundException
+     * @param \Closure $next
      *
      * @return mixed
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *
+     * @throws \Illuminate\Container\EntryNotFoundException
      */
     public function handle($message, \Closure $next)
     {
